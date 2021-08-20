ea644d090e3a5240ddb8a48f13f88a93ec03eac5
JGutierrez1457 <jgutierrezch@uni.pe>
2021-08-20 16:18:28 -0500

    Added validate when iditem dont exist

--- controllers/items.controller.js
+++ controllers/items.controller.js
@@ -15,12 +15,14 @@ itemsController.buyItem = async(req, res)=>{//Middleware para comprar #quantity 
     const { quantity } = req.body;//Se recupera la cantidad que se desea comprar
     try {
         const result = await itemsDAO.getItemById(idItem);//Consulta existencia de item por su id
+        if(!result.length)return res.status(404).send('No existe el item')//Retorna error si no se encuentra un item con el id proporcionado
         const item = result[0].dataValues;//Se extrar el objeto del item
         if(quantity > item.stock)return res.status(400).send('Cantidad excede el stock');//Si la cantidad solicitada a comprar excede el stock retorna error
-        await itemsDAO.updateItem({ stock : item.stock - quantity },idItem)//Modifica el stock del item restandole la cantidad solicitada
+        await itemsDAO.updateItemById({ stock : item.stock - quantity },idItem)//Modifica el stock del item restandole la cantidad solicitada
         const itemUpdated = await itemsDAO.getItemById(idItem);//Consulta objecto modificado del item
         return res.status(200).json(itemUpdated[0].dataValues)//Se responde el objeto del item
     } catch (error) {
+        console.log(error)
         return res.status(500).send(error)
     }
 }
