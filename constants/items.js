f8263c952e72491b147afa86f62f6f4521474da5
JGutierrez1457 <jgutierrezch@uni.pe>
2021-08-20 16:42:25 -0500

    Added items

--- constants/items.js
+++ constants/items.js
@@ -1,8 +1,14 @@
 module.exports = [
-    { name : 'lechuga', description : 'Verde y fresca', stock : 20, category : 'verduras', price : 2.2, image : 'https://api-tiendalilab.s3.amazonaws.com/lechuga.jpg' },
-    { name : 'tomate', description : 'Roja y fresca', stock : 10, category : 'verduras', price : 2.5, image : 'https://api-tiendalilab.s3.amazonaws.com/tomate.jpg' },
-    { name : 'brocoli', description : 'Verde y nutritiva', stock : 30, category : 'verduras', price : 5, image : 'https://api-tiendalilab.s3.amazonaws.com/brocoli.jpg' },
-    { name : 'platano', description : 'Amarillo y nutritivo', stock : 15, category : 'frutas', price : 1.5, image : 'https://api-tiendalilab.s3.amazonaws.com/platano.jpg' },
-    { name : 'palta', description : 'Verde y madura', stock : 12, category : 'frutas', price : 3.5, image : 'https://api-tiendalilab.s3.amazonaws.com/palta.jpg' },
-    { name : 'tuna', description : 'Fresca y andina', stock : 5, category : 'frutas', price : 7, image : 'https://api-tiendalilab.s3.amazonaws.com/tuna.jpg' }
+    { name : 'Lechuga', description : 'Verde y fresca', stock : 20, category : 'verduras', price : 2.2, image : 'https://api-tiendalilab.s3.amazonaws.com/lechuga.jpg' },
+    { name : 'Tomate', description : 'Roja y fresca', stock : 10, category : 'verduras', price : 2.5, image : 'https://api-tiendalilab.s3.amazonaws.com/tomate.jpg' },
+    { name : 'Brocoli', description : 'Verde y nutritiva', stock : 30, category : 'verduras', price : 5, image : 'https://api-tiendalilab.s3.amazonaws.com/brocoli.jpg' },
+    { name : 'Platano', description : 'Amarillo y nutritivo', stock : 15, category : 'frutas', price : 1.5, image : 'https://api-tiendalilab.s3.amazonaws.com/platano.jpg' },
+    { name : 'Palta', description : 'Verde y madura', stock : 12, category : 'frutas', price : 3.5, image : 'https://api-tiendalilab.s3.amazonaws.com/palta.jpg' },
+    { name : 'Tuna', description : 'Fresca y andina', stock : 5, category : 'frutas', price : 7, image : 'https://api-tiendalilab.s3.amazonaws.com/tuna.jpg' },
+    { name : 'Fresa', description : 'Roja y fresca', stock : 15, category : 'frutas', price : 17, image : 'https://api-tiendalilab.s3.amazonaws.com/fresa.jpg' },
+    { name : 'Esparrago', description : 'Verde y nutritiva', stock : 3, category : 'verduras', price : 3, image : 'https://api-tiendalilab.s3.amazonaws.com/esparrago.jpg' },
+    { name : 'Cereza', description : 'Roja y dulce', stock : 6, category : 'frutas', price : 4, image : 'https://api-tiendalilab.s3.amazonaws.com/cereza.jpg' },
+    { name : 'Cebolla', description : 'Fresca y andina', stock : 11, category : 'verduras', price : 2, image : 'https://api-tiendalilab.s3.amazonaws.com/cebolla.jpg' },
+    { name : 'Apio', description : 'Verde y nutritiva', stock : 12, category : 'verduras', price : 1.3, image : 'https://api-tiendalilab.s3.amazonaws.com/apio.jpg' },
+    { name : 'Sandilla', description : 'Grande y fresca', stock : 15, category : 'frutas', price : 7, image : 'https://api-tiendalilab.s3.amazonaws.com/sandilla.jpg' }
 ]
